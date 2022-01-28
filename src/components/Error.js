38b368e6411e14593a2f238cc51a4904587853eb
selimmh <selim.mht@gmail.com>
2022-01-28 16:26:07 +0200

    .

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 function Error() {
   let navigate = useNavigate();
   useEffect(() => {
-    navigate("/");
+    navigate("/selimmh-me");
   }, []);
   return (
     <div className="w-full h-screen flex justify-center items-center">
